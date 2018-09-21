aacb41a2987fc0371ed8d719749801d687ee7564
Ludwig <lm@ludwigmair.de>
2018-09-22 00:43:13 +0200

    Fehler für Chaos behoben (if xxx == False or NotImplemented: ...)

--- bot.py
+++ bot.py
@@ -212,7 +212,7 @@ for i in range(0, len(tasks0s)):
         task0 = tasks0s[i][task_keys[j]]
         task1 = tasks1s[i][task_keys[j]]
 
-        if task0.__eq__(task1) == False or NotImplemented:
+        if task0.__eq__(task1) == False:
 
             #VORBEREITUNG/VEREINFACHUNG:
             key_which_has_changed = task_keys[j]
@@ -329,15 +329,15 @@ print(bot_message_task_changed)
 
 
 
-# ##########
-# ## Chats werden abgeschickt (NEUE TASK)
-# for i in range (0, len(bot_message_new_task)):
-#     send_message(str(bot_message_new_task[i]), which_group[bot_message_new_task_dict[bot_message_new_task[i]]])
-#
-# ## Chats werden abgeschickt (VERÄNDERTE TASK)
-#
-# for i in range (0, len(bot_message_task_changed)):
-#     send_message(str(bot_message_task_changed[i]), which_group[bot_message_task_changed_dict[bot_message_task_changed[i]]])
+##########
+## Chats werden abgeschickt (NEUE TASK)
+for i in range (0, len(bot_message_new_task)):
+    send_message(str(bot_message_new_task[i]), which_group[bot_message_new_task_dict[bot_message_new_task[i]]])
+
+## Chats werden abgeschickt (VERÄNDERTE TASK)
+
+for i in range (0, len(bot_message_task_changed)):
+    send_message(str(bot_message_task_changed[i]), which_group[bot_message_task_changed_dict[bot_message_task_changed[i]]])
 
 
 
